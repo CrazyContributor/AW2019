ef0f8fc85fd3d40c62da4cf68c5dea5de7d8ae2d
CRaZY <cr4zylo@yahoo.com>
2022-06-29 19:54:47 +0200

    Commit

---
+++ Tables/StaticData/dbo.Categories [Data].sql
@@ -0,0 +1,10 @@
+SET IDENTITY_INSERT [dbo].[Categories] ON
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (1, N'Beverages')
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (2, N'Condiments')
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (3, N'Confections')
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (4, N'Dairy Products')
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (5, N'Grains/Cereals')
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (6, N'Meat/Poultry')
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (7, N'Produce')
+INSERT INTO [dbo].[Categories] ([CategoryID], [CategoryName]) VALUES (8, N'Seafood')
+SET IDENTITY_INSERT [dbo].[Categories] OFF
---
+++ Tables/StaticData/dbo.Shippers [Data].sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Shippers] ON
+INSERT INTO [dbo].[Shippers] ([ShipperID], [CompanyName], [Phone]) VALUES (1, N'Speedy Express', N'(503) 555-9831')
+INSERT INTO [dbo].[Shippers] ([ShipperID], [CompanyName], [Phone]) VALUES (2, N'United Package', N'(503) 555-3199')
+INSERT INTO [dbo].[Shippers] ([ShipperID], [CompanyName], [Phone]) VALUES (3, N'Federal Shipping', N'(503) 555-9931')
+SET IDENTITY_INSERT [dbo].[Shippers] OFF
