8a4e23f7d8ee847cc524541e1208b1c3eb8e53a9
CRaZY <cr4zylo@yahoo.com>
2022-06-29 19:55:28 +0200

    Commit

--- Tables/StaticData/dbo.Region [Data].sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription]) VALUES (1, N'Eastern                                           ')
-INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription]) VALUES (2, N'Western                                           ')
-INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription]) VALUES (3, N'Northern                                          ')
-INSERT INTO [dbo].[Region] ([RegionID], [RegionDescription]) VALUES (4, N'Southern                                          ')
