56d4623607773ddf7b974685e03d47388babb0d3
DESKTOP-DD6KN2K\NiNjA <ddvisioncontributor3@gmail.com>
2023-05-15 20:57:34 +0200

    commit

--- Views/dbo.Customer and Suppliers by City.sql
+++ Views/dbo.Customer and Suppliers by City.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 
-create view "Customer and Suppliers by City" AS
+create view "Customer1 and Suppliers by City" AS
 SELECT City, CompanyName, ContactName, 'Customers' AS Relationship 
 FROM Customers
 UNION SELECT City, CompanyName, ContactName, 'Suppliers'
