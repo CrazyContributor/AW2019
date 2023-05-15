4206c059e8a93681c2c2b82312aeca7e5705efcb
CrazyContributor <91538756+CrazyContributor@users.noreply.github.com>
2023-05-15 21:15:37 +0200

    Update dbo.Summary of Sales by Quarter.sql

--- Views/dbo.Summary of Sales by Quarter.sql
+++ Views/dbo.Summary of Sales by Quarter.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 
-create view "Summary of Sales by Quarter" AS
+create view "Summary of Sales 1by Quarter" AS
 SELECT Orders.ShippedDate, Orders.OrderID, "Order Subtotals".Subtotal
 FROM Orders INNER JOIN "Order Subtotals" ON Orders.OrderID = "Order Subtotals".OrderID
 WHERE Orders.ShippedDate IS NOT NULL
