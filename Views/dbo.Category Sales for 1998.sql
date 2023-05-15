f884f9d3136bf48439502b194b5aab7d5379ac6c
CrazyContributor <91538756+CrazyContributor@users.noreply.github.com>
2023-05-15 21:13:30 +0200

    Update dbo.Category Sales for 1998.sql

--- Views/dbo.Category Sales for 1998.sql
+++ Views/dbo.Category Sales for 1998.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 
-create view "Category Sales for 1997" AS
+create view "Category Sales for 1998" AS
 SELECT "Product Sales for 1997".CategoryName, Sum("Product Sales for 1997".ProductSales) AS CategorySales
 FROM "Product Sales for 1997"
 GROUP BY "Product Sales for 1997".CategoryName
