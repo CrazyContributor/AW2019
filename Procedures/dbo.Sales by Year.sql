e2a081d4c82777d8d1845e4443a5f2e9c23ba8bf
CrazyContributor <ddvisioncontributor3@gmail.com>
2023-12-25 20:52:49 +0100

    Commit

--- Procedures/dbo.Sales by Year.sql
+++ Procedures/dbo.Sales by Year.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 
-create procedure "Sales by Year" 
+create procedure "Sales by Years" 
 	@Beginning_Date DateTime, @Ending_Date DateTime AS
 SELECT Orders.ShippedDate, Orders.OrderID, "Order Subtotals".Subtotal, DATENAME(yy,ShippedDate) AS Year
 FROM Orders INNER JOIN "Order Subtotals" ON Orders.OrderID = "Order Subtotals".OrderID
